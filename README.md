176efd3e3fd441005ff427a30b5f14feb6072362
sena <senaredline@gmail.com>
2017-11-19 10:11:27 +0900

    first commit
    消してしまったのを直した

---
+++ README.md
@@ -0,0 +1,14 @@
+# webChat  
+簡単なwebアプリケーションです。  
+webページを開きなおすとidが再発行されてしまうため、ユーザー名を保持することはできません。  
+## モジュール  
+express  
+moment  
+socket.io  
+## 使い方  
+1.`git clone https://github.com/SenaSuzuki/webChat.git`でクローンします。  
+2.`cd webChat && npm install`します。  
+3.`npm start`で起動します。  
+4.ブラウザで`http://localhost:80/`でchatに入ることができます。  
+## ライセンス  
+MIT  
---
+++ app.js
@@ -0,0 +1,44 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var moment = require('moment');
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+var userName = new Array();
+
+io.on('connection', function (socket) {
+  socket.broadcast.emit('system message', "一人入室しました")
+  var id = socket.id;
+
+  if (userName[id] == undefined) {
+    socket.emit('system message', "setName 名前　でユーザー名を設定できます");
+  }
+
+  socket.on('set name', function (name) {
+    userName[id] = name;
+    socket.emit('system message', "名前を設定しました" + userName[id]);
+  });
+
+  socket.on('chat message', function (msg) {
+    var now = moment().format('HH:mm');
+    var name;
+    if (userName[id] == undefined) {
+      name = "noName";
+    } else
+      name = userName[id];
+    io.emit('chat message', now + " " + name + " " + msg);
+  });
+
+  socket.on('disconnect', function () {
+    socket.broadcast.emit('system message', "一人退室しました");
+  })
+});
+
+
+
+http.listen(80, function () {
+  console.log('Server running...');
+});
