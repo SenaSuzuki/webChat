aef7b0fc6e72b17bf648eca7a82a71de4830df96
sena <senaredline@gmail.com>
2018-02-19 22:04:38 +0900

    0.0.1

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/chat', function (req, res) {
+  res.sendFile(__dirname + '/chat.html');
+});
+
 var userName = new Array();
 
 io.on('connection', function (socket) {
